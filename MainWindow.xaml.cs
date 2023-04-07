f4b2197894ea9a33239ffee90469b590a3c981e8
anttiastikainen <antti.astikainen@edu.karelia.fi>
2023-04-07 20:05:56 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Configuration;
+
+namespace WeatherApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateView("Joensuu,fi");
+            Console.WriteLine("Hello world!");
+        }
+        private async void UpdateView(string location)
+        {
+            // Enter your OpenWeatherMap API key here
+            string apiKey = ConfigurationManager.AppSettings["ApiKey"];
+
+            // Create a new instance of WeatherApiClient
+            var client = new WeatherApiClient(apiKey);
+
+            // Get the weather data for the given location
+            var weather = await client.GetWeatherAsync(location);
+
+            // Print the weather data to the console
+            System.Diagnostics.Debug.WriteLine($"Location: {weather.Name}");
+            System.Diagnostics.Debug.WriteLine($"Temperature: {weather.Main.Temp}°C");
+            System.Diagnostics.Debug.WriteLine($"Humidity: {weather.Main.Humidity}%");
+            System.Diagnostics.Debug.WriteLine($"Feels like: {weather.Main.FeelsLike}°C");
+            System.Diagnostics.Debug.WriteLine($"Min: {weather.Main.TempMin}°C");
+            System.Diagnostics.Debug.WriteLine($"Max: {weather.Main.TempMax}°C");
+
+            // Update Textblocks
+            SelectedCityTextBlock.Text=weather.Name;
+            string temp = ($"Temperature: {weather.Main.Temp}°C");
+            TemperatureTextBlock.Text = temp;
+            string humid = ($"Humidity: {weather.Main.Humidity}%");
+            HumidityTextBlock.Text = humid;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            var location = (string)button.Tag;
+            UpdateView(location);
+        }
+    }
+}
---
+++ WeatherApiClient.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+public class Weather
+{
+    public string Name { get; set; }
+    public WeatherDescription[] WeatherDescriptions { get; set; }
+    public MainData Main { get; set; }
+}
+
+public class WeatherDescription
+{
+    public string Description { get; set; }
+}
+
+public class MainData
+{
+    [JsonProperty("temp")]
+    public float Temp { get; set; }
+    [JsonProperty("feels_like")]
+    public float FeelsLike { get; set; }
+    [JsonProperty("temp_min")]
+    public float TempMin { get; set; }
+    [JsonProperty("temp_max")]
+    public float TempMax { get; set; }
+    [JsonProperty("pressure")]
+    public float Pressure { get; set; }
+    [JsonProperty("humidity")]
+    public float Humidity { get; set; }
+}
+
+public class WeatherApiClient
+{
+
+    private readonly HttpClient _httpClient;
+    private readonly string _apiKey;
+
+    public WeatherApiClient(string apiKey)
+    {
+        _httpClient = new HttpClient();
+        _apiKey = apiKey;
+    }
+
+    public async Task<Weather> GetWeatherAsync(string location)
+    {
+        var apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={_apiKey}&units=metric";
+        var response = await _httpClient.GetAsync(apiUrl);
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var weather = JsonConvert.DeserializeObject<Weather>(responseContent);
+        return weather;
+    }
+
+}
